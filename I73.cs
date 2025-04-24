fa6720715e9b5464a2e2f6d1b17b7edca12ea2c8
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-04-24 09:06:01 +0500

    Create I73.cs

---
+++ I73.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        if (strs.Length == 0) return "";
+        string prefix = strs[0];
+        foreach (string s in strs)
+            while (!s.StartsWith(prefix))
+                prefix = prefix.Substring(0, prefix.Length - 1);
+        return prefix;
+    }
+}
