5e3480f2285a15508ff1c60b89c3e732d3eee778
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-04-25 08:46:04 +0500

    Create I74.cs

---
+++ I74.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public long CountInterestingSubarrays(IList<int> nums, int modulo, int k) {
+        var map = new Dictionary<int, long> { [0] = 1 };
+        long res = 0;
+        int count = 0;
+
+        foreach (var num in nums) {
+            if (num % modulo == k) count++;
+            int curr = count % modulo;
+            int need = (curr - k + modulo) % modulo;
+            if (map.ContainsKey(need)) res += map[need];
+            if (!map.ContainsKey(curr)) map[curr] = 0;
+            map[curr]++;
+        }
+        return res;
+    }
+}
