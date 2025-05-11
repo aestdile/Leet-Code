0707727230a14b5df59770b92e0642b2ba9508b4
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-05-11 08:35:25 +0500

    Create I90.cs

---
+++ I90.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool ThreeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        foreach (int num in arr) {
+            if (num % 2 == 1) {
+                count++;
+                if (count == 3) return true;
+            } else {
+                count = 0; 
+            }
+        }
+        return false; 
+    }
+}
