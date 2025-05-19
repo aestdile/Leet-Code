be27cdf71fb9dc1d18cb0b111ad3ec229292e1ab
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-05-19 13:01:25 +0500

    Update I98.cs

--- I98.cs
+++ I98.cs
@@ -1,15 +1,9 @@
 public class Solution {
-    public string TriangleType(int[] nums) {
-        Array.Sort(nums); 
-        if (nums[0] + nums[1] <= nums[2]) {
-            return "none";
-        }
-        if (nums[0] == nums[1] && nums[1] == nums[2]) {
-            return "equilateral";
-        }
-        if (nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]) {
-            return "isosceles";
-        }
+    public string TriangleType(int[] n) {
+        Array.Sort(n);
+        if (n[0] + n[1] <= n[2]) return "none";
+        if (n[0] == n[2]) return "equilateral";
+        if (n[0] == n[1] || n[1] == n[2]) return "isosceles";
         return "scalene";
     }
 }
