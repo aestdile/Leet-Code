59c2151e522b3155cbcdcf1448f4753883924cc5
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-06-28 18:43:55 +0500

    Create I138.cs

---
+++ I138.cs
@@ -0,0 +1,13 @@
+public class Solution 
+{
+    public int[] MaxSubsequence(int[] nums, int k) 
+    {
+        List<int> list = new(nums);
+        int n = nums.Length - k;
+        for (int i = 0; i < n; i++) 
+        {
+            list.RemoveAt(list.IndexOf(list.Min()));
+        }
+        return list.ToArray();
+    }
+}
