29773c34a61f63fe7eeeee6f7262dfdb1d26bcf7
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-07-01 10:56:59 +0500

    Create I149.cs

---
+++ I149.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int PossibleStringCount(string word) {
+        int n = word.Length, ans = 1;
+        for (int i = 1; i < n; ++i)
+        {
+            if (word[i - 1] == word[i])
+            {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
