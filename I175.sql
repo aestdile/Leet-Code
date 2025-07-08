132cbf62e1d212cc62564c8cf166ef0c548adb83
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-07-08 08:47:34 +0500

    Create I175.sql

---
+++ I175.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.product_name AS product_name,
+    SUM(o.unit) AS unit
+FROM Products p
+INNER JOIN Orders o
+ON p.product_id = o.product_id
+WHERE o.order_date LIKE "2020-02-%"
+GROUP BY p.product_name
+HAVING SUM(o.unit) >= 100
