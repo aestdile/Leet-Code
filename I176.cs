45b0e8f5f2a0c7c1734ff3717b302a83697623f1
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-07-08 08:49:32 +0500

    Create I176.cs

---
+++ I176.cs
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    bool traverse(TreeNode p,TreeNode q){
+        if(p==null&&q==null){
+            return true;
+        }
+        if(p==null||q==null){
+            return false;
+        }
+        if(p.val!=q.val){
+            return false;
+        }
+        bool ok1=traverse(p.left,q.left);
+        bool ok2=traverse(p.right,q.right);
+        return ok1&&ok2?true:false;
+    }
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        return traverse(p,q);
+    }
+}
