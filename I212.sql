1566bcfa9e1e7c43cc64e06b7176cc272c34052a
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-07-25 23:24:51 +0500

    Create I212.sql

---
+++ I212.sql
@@ -0,0 +1,5 @@
+select p.product_id,p.product_name from Product as p
+join Sales as s
+on p.product_id=s.product_id
+group by s.product_id
+having min(s.sale_date) >='2019-01-01' and max(s.sale_date)<='2019-03-31'
