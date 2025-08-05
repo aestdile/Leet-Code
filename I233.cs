864fb74aa6fbb1e0144a6e9e2045cd13c75670ff
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-08-05 18:32:11 +0500

    Create I233.cs

---
+++ I233.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int NumOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int ans = 0;
+
+        for (int i = 0; i < fruits.Length; i++)
+        {
+            bool check = false;
+            for (int j = 0; j < baskets.Length; j++)
+            {
+                if (fruits[i] <= baskets[j])
+                {
+                    check = true;
+                    baskets[j] = 0;
+                    break;
+                }
+            }
+            if (check == false)
+            {
+                ans += 1;
+            }
+        }
+        return ans;
+    }
+}
