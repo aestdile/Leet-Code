cc173c3dbe2f4afa5bf477e07b6b6c08ba9a46a0
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-09-07 12:02:41 +0500

    Create I274.cs

---
+++ I274.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] SumZero(int n) {
+        int[] arr = new int[n];
+        int k = 1;
+
+        for (int i = 0; i < n / 2; i++) {
+            arr[i] = k;
+            arr[n - 1 - i] = -k;
+            k++;
+        }
+        return arr;
+    }
+}
