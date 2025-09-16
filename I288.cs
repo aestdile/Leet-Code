a207e065f44c618e5a9320e681e31d1c5b65ad65
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-09-16 12:50:51 +0500

    Create I288.cs

---
+++ I288.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public IList<int> ReplaceNonCoprimes(int[] nums) {
+        var result = new List<int>();
+
+        foreach (int num in nums) {
+            result.Add(num);
+            while (result.Count > 1) {
+                int a = result[result.Count - 1];
+                int b = result[result.Count - 2];
+                int g = GCD(a, b);
+                if (g > 1) {
+                    result.RemoveAt(result.Count - 1);
+                    result.RemoveAt(result.Count - 1);
+                    long lcm = (long)a / g * b; 
+                    result.Add((int)lcm);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int GCD(int a, int b) {
+        while (b != 0) {
+            int tmp = b;
+            b = a % b;
+            a = tmp;
+        }
+        return a;
+    }
+}
