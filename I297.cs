1b02686931f8ce882840001a7d819c66d7dac3d3
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-09-19 10:13:56 +0500

    Create I297.cs

---
+++ I297.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public string[] FindWords(string[] words)
+        => words.Where(w => _rows.Any(l => w.ToLower().All(c => l.Contains(c)))).ToArray();
+
+    static string[] _rows = new string[]
+    {
+        "qwertyuiop",
+        "asdfghjkl",
+        "zxcvbnm",
+    };
+}
