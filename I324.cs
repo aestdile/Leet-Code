b802a367bf395e0c67e31f3600f2074100b311fa
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-10-10 12:40:26 +0500

    Create I324.cs

---
+++ I324.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MaximumEnergy(int[] energy, int k) {
+        int n = energy.Length;
+        int[] dp = new int[n];
+        int result = int.MinValue;
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = energy[i] + ((i + k < n) ? dp[i + k] : 0);
+            result = Math.Max(result, dp[i]);
+        }
+        return result;
+    }
+}
