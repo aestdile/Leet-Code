e811af4d7cdf3ae9f0ee34ff2b7eb973afd73ea0
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-12-05 17:43:12 +0500

    Create I407.cs

---
+++ I407.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MinDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else if (root.left == null && root.right == null) {
+            return 1;
+        } else {
+            int min = 1000000;
+            if (root.left != null) {
+                min = Math.Min(min, MinDepth(root.left));
+            }
+            if (root.right != null) {
+                min = Math.Min(min, MinDepth(root.right));
+            }
+            return min + 1;
+        }
+    }
+}
