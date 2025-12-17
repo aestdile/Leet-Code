453334bb1e20966b98becd333bfce8f342c2878e
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2025-12-17 22:16:19 +0500

    Create I429.cs

---
+++ I429.cs
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution{
+    public int CountNodes(TreeNode n) =>
+        n is null ? 0 : 1 + CountNodes(n.left) + CountNodes(n.right);
+}
