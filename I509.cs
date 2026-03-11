c7c44a7d770da25a07f106b65673e4c098bf3733
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2026-03-11 22:25:33 +0500

    Implement BitwiseComplement method in Solution class

---
+++ I509.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int BitwiseComplement(int n) {
+        string bin = Convert.ToString(n, 2);
+        string rev = "";
+
+        foreach (char c in bin)
+        {
+            if (c == '1'){
+                rev += '0';
+            }     
+            else{
+                rev += '1';
+            }        
+        }
+
+        return Convert.ToInt32(rev, 2);
+    }
+}
