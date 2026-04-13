2420f8aff7b84b4fa59f60fd039616bb8b9c4e02
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2026-04-13 06:32:05 +0500

    Implement GetMinDistance method in Solution class

---
+++ I542.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int GetMinDistance(int[] nums, int target, int start) {
+        int minDistance = int.MaxValue;
+        
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == target) {
+                int distance = Math.Abs(i - start);
+                if (distance < minDistance) {
+                    minDistance = distance;
+                }
+            }
+        }
+        
+        return minDistance;
+    }
+}
